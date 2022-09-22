6e55c1b7dd67ea8666725532eb55d4c88cb2c587
Naivedya Agarwal <naivedyaagarwal2003@gmail.com>
2022-09-22 11:19:33 +0530

    nodejs project commit

--- index.js
+++ index.js
@@ -24,19 +24,22 @@ const { dirname } = require("path");
 app.use('/static' , express.static("static"))
 const users = [];
 const usersId = [];
-var onlineUserNo =0
+var onlineUserNo =0;
+var address;
 
 // when connection comes(ie harry , rohan ...) to socket.io then "io.on" will listen these
 // and whenever something happens with a particular connection(ie rohan) then what should be done with that particular connection is handled by "socket.on" so if a "new-user-joined" event(note: it is our wish whatever name we want to keep here we kept "new-user-joined") comes to socket.on then the callback function is run which takes the argument "socket"
 io.on("connection", socket=>{
-    // var address = socket.handshake.address;
-    // console.log('New connection from ' + address.address + ':' + address.port);
+    address = socket.handshake.address;
+    console.log('New connection from ' + address.address + ':' + address.port);
+    IPAddress=address.address + ':' + address.port;
+
     // socket.on will receive a message or event from the client javascript 
     // socket.on("hello from client", (...args) =>{ }) here name is argument
     socket.on('new-user-joined', name=>{
         // console.log("New user",name);
         // when new user joins it will add the user name in users array
-        usersId[onlineUserNo]= socket.id
+        usersId[onlineUserNo]= IPAddress
         users[onlineUserNo] = name
         onlineUserNo += 1
         // socket.broadcast.emit() functon will emit or send a message(event) to all the client's javascript(except the client that caused it) 'user-joined' with name as argument
@@ -46,8 +49,8 @@ io.on("connection", socket=>{
     });
     // if socket get an event whose name we kept as 'send' from the client javascript then
     socket.on('send' , message=>{
-        // here user[socket.id] will give the name of the client from user array which we created before and we are keeping all the values as object
-        socket.broadcast.emit('recieve', {message: message, name: users[usersId.indexOf(socket.id)] })
+        // here user[IPAddress] will give the name of the client from user array which we created before and we are keeping all the values as object
+        socket.broadcast.emit('recieve', {message: message, name: users[usersId.indexOf(IPAddress)] })
     });
     // when a client diconnects(this event is automatically triggered by socket.io as soon as the client disconnects)
     socket.on('disconnect', message=>{
